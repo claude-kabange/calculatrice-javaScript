332c7b9dc06f3e5dfc57198cb218d85d4aa36d8e
claude-kabange <20kw086@esisalama.org>
2022-09-02 12:39:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function sin() {
+    document.calcul.result.value = Math.sin(document.calcul.result.value);
+}
+
+function cos() {
+    document.calcul.result.value = Math.cos(document.calcul.result.value);
+}
+
+function tan() {
+    document.calcul.result.value = Math.tan(document.calcul.result.value);
+}
+
+function BACKSPC() {
+    var a = document.calcul.result.value;
+    document.calcul.result.value = a.substr(0, a.length - 1);
+}
+
+
+function number(value) {
+    document.calcul.result.value += value;
+}
+
+function remv() {
+    document.calcul.result.value = " ";
+}
+
+function equal() {
+    document.calcul.result.value = eval(document.calcul.result.value);
+}
